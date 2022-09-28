6c0276e0bfbac2913a6bbc98ed5ced01caac20e2
Ryan Vaughan <ryvaughan17@gmail.com>
2022-09-27 22:37:42 -0400

    added simple login check

--- chess_plus_plus/front_end/src/setup/SignInPage.js
+++ chess_plus_plus/front_end/src/setup/SignInPage.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import serverURL from "../config/serverConfig";
 
+
 function SignInPage({setIsLoggedIn}) {
+
     const cognitoUrl = 'https://chessplusplus.auth.us-east-1.amazoncognito.com/login?client_id=39i33g2381dako8dicf0nd5hdl&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=http://localhost:3000/login'
+
+    useEffect(() => {
+        console.log('yooo');
+        if (hasFragmentIdentifier()) {
+            setIsLoggedIn(true);
+        }
+        else {
+            setIsLoggedIn(false);
+        }
+    });
+    const hasFragmentIdentifier = () => {
+        let url = window.location.href;
+        return url.includes('#');
+    }
+
 
     const doOtherThing = () => {
         setIsLoggedIn(true);

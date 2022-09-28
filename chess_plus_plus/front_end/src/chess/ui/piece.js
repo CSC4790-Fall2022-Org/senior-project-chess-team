e6dee309212aa41325c154a4ba76fc33476af4e6
cschmi14 <cschmidt542@gmail.com>
2022-09-27 20:23:51 -0500

    added valid move highlights on squares

--- chess_plus_plus/front_end/src/chess/ui/piece.js
+++ chess_plus_plus/front_end/src/chess/ui/piece.js
@@ -26,7 +26,7 @@ export default function Piece(props) {
     return (
         <>
         <DndProvider backend={HTML5Backend}>
-            <DragPreviewImage connect={preview} src={imageUrl}/>
+            <DragPreviewImage connect={preview} src={imageUrl} />
             <div class={props.state.boardState.playerIsWhite === piece.isWhite ? 'my-piece' : 'opp-piece'}
             style={{backgroundImage: `url(${imageUrl})`, opacity: `${isDragging ? 0 : 1}`}}
             ref={props.state.boardState.playerIsWhite === piece.isWhite ? drag : null}>
--- chess_plus_plus/front_end/src/chess/ui/square.js
+++ chess_plus_plus/front_end/src/chess/ui/square.js
@@ -8,15 +8,10 @@ export default function Square({piece, pos, state, updateGame}) {
 
   // When piece is dropped, check if it can be moved there, then move it
   // and send to backend
-  const [, drop] = useDrop({
+  const [{isOver, canDrop}, drop] = useDrop({
     accept: 'Piece',
     drop: (item) => {
       const src_pos = item.id.split("_")[0];
-      let isWhite = item.id.split("_")[2];
-      isWhite = isWhite === "true" ? true : false;
-      if (isWhite !== state.boardState.playerIsWhite) {
-        return;
-      }
       const dest_pos= pos;
       console.log(src_pos + " to " + dest_pos);
       const validMove = state.boardState.movePiece(src_pos, dest_pos);
@@ -24,18 +19,51 @@ export default function Square({piece, pos, state, updateGame}) {
         updateGame(state.boardState);
       }
       // SEND THE UPDATE TO THE BACKEND HERE
-    }
+    },
+    canDrop: (item) => {
+      const src_pos = item.id.split("_")[0];
+      const dest_pos= pos;
+      return state.boardState.canMovePiece(src_pos, dest_pos)
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop()
+    })
   })
+  let color = ""
+    if (isOver && !canDrop) {
+        color = 'rgba(255, 0, 0, .6)';
+    }
+    else if (isOver && canDrop) {
+        color = 'rgba(0, 255, 0, .6)';
+    }
+    else {
+        color = 'rgba(255, 255, 0, .6)';
+    }
+
   if (piece !== null) {
       return (
-        <div class='square' ref={drop}>
+        <div class='square' style={{
+          height: '80px',
+          width: '80px',
+          zIndex: 1,
+          backgroundColor: `${canDrop || isOver ? color : ""}`
+        }}
+         ref={drop}>
           <Piece piece={piece} pos = {pos} state= {state}></Piece>
         </div>
       )
   }
   else {
     return (
-      <div class='square' ref={drop}>
+      <div class='square' 
+        style={{
+          height: '80px',
+          width: '80px',
+          zIndex: 1,
+          backgroundColor: `${canDrop || isOver ? color : ""}`
+        }}
+        ref={drop}>
       </div>
     )
   }

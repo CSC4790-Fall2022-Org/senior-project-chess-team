38dd13792879e28cca7a53ad107c8e4453b1d0f4
jwsanft2019 <jwsanft2019@gmail.com>
2022-10-02 15:56:24 -0400

    first attempt at token verification

--- chess_plus_plus/server/authentication/checkAuthenticity.js
+++ chess_plus_plus/server/authentication/checkAuthenticity.js
@@ -1,14 +1,34 @@
+import {CognitoJwtVerifier} from "aws-jwt-verify";
+
 const axios = require('axios')
 
 const checkAuthenticity = async (body) => {
+    var jwt = require('jsonwebtoken');
+    var jwkToPem = require('jwk-to-pem');
+    var pem = jwkToPem(jwk);
+    jwt.verify(token, pem, { algorithms: ['RS256'] }, function(err, decodedToken) {});
     let jwts = await fetchJwtFromAws(body);
     if (jwts == null) {
         return {'error': 'could not authenticate with AWS'}
+    }
+    const verifier = CognitoJwtVerifier.create({
+        userPoolId: "<user_pool_id>",
+        tokenUse: "access",
+        clientId: "<client_id>",
+    });
+    try {
+        const payload = await verifier.verify(
+            "39i33g2381dako8dicf0nd5hdl" //JWT as string
+        );
+        console.log("Token Valid. Payload: ", payload);
+    } catch {
+        console.log("Invalid Token");
     }
     // TODO: Verify these JWT's with AWS. https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-using-tokens-verifying-a-jwt.html
     // https://stackoverflow.com/questions/40302349/how-to-verify-jwt-from-aws-cognito-in-the-api-backend
     // https://aws.amazon.com/premiumsupport/knowledge-center/decode-verify-cognito-json-token/
     // above may be useful
+    
     return {"id_token": jwts.id_token};
 }
 

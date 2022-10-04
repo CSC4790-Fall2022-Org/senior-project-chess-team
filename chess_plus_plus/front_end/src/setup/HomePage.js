47a61f2ac852625ae7620fa6c0d47ed21df91bdd
Ryan Vaughan <ryvaughan17@gmail.com>
2022-10-04 19:14:31 -0400

    add support to transmit id token to backend

--- chess_plus_plus/front_end/src/setup/HomePage.js
+++ chess_plus_plus/front_end/src/setup/HomePage.js
@@ -1,4 +1,5 @@
 import { useSearchParams } from "react-router-dom";
+import { createGameRoom } from "../api/gameRoom.js";
 import {Game} from '../chess/ui/game.js'
 
 function HomePage({setIsLoggedIn}) {
@@ -7,10 +8,20 @@ function HomePage({setIsLoggedIn}) {
         setIsLoggedIn(false);
         localStorage.removeItem('id_token');
     }
+
+    const createGame = async () => {
+        const response = await createGameRoom();
+    }
+
+    const joinGame = () => {
+
+    }
     return (
         <>
         <h1>home </h1>
         <button onClick={pseudoLogout}> logout </button>
+        <button onClick={createGame}>Create game</button>
+        <button onClick={joinGame}>Join game</button>
         <Game isWhite={false}></Game>
         </>
     )
--- chess_plus_plus/server/server.js
+++ chess_plus_plus/server/server.js
@@ -8,7 +8,7 @@ const checkAuthenticity = require('./authentication/checkAuthenticity.js')
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 
 })
@@ -20,15 +20,17 @@ app.get('/hello', (req, res) => {
 
 app.post('/authenticate', async (req, res) => {
   requestBody = req.body
-
   let text = await checkAuthenticity.checkAuthenticity(requestBody);
-
   if (text?.error) {
     return res.status(400).send(text) // should handle error differently, but okay for now
   }
   return res.status(200).send(text)
 })
 
+app.post('/game', (req, res) => {
+  console.log('we start')
+  console.log(req.headers['authorization']) // todo : actually call from frontend
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

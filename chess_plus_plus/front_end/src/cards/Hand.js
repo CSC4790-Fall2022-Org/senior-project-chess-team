cf3ddd3491dc067a7f9b536db1ef378137e646f0
Ryan Vaughan <ryvaughan17@gmail.com>
2022-11-11 12:54:08 -0500

    frontend renders correct id from backend

--- chess_plus_plus/front_end/src/cards/Hand.js
+++ chess_plus_plus/front_end/src/cards/Hand.js
@@ -2,14 +2,13 @@ import Card from "./Card";
 
 function Hand({cards, ws, id}) {
     console.log('hand rendered')
-    let cardId = 0
 
     function useCard(cardId) {
         console.log('use card with id', cardId);
     }
     return (
         <div id='hand'>
-            {cards.map(item => <Card name={item.name} description={item.description} id={cardId++} useCard={useCard}/>)}
+            {cards.map(item => <Card name={item.name} description={item.description} id={item.id} useCard={useCard}/>)}
         </div>
     )
 }

23f6ed4a9baabe7b456bf0e80031d6b6fdd24a11
Ryan Vaughan <ryvaughan17@gmail.com>
2022-12-04 15:04:17 -0500

    images on cards

--- chess_plus_plus/front_end/src/cards/Card.js
+++ chess_plus_plus/front_end/src/cards/Card.js
@@ -1,16 +1,39 @@
-import x from '../chess/files/pieces/wR.png' 
-import css from './card.css'
+import './card.css'
+
+import frozenCrossImage from './cardImages/cross.png'
+import reviveImage from './cardImages/revive.png'
+import snowflakeImage from './cardImages/snowflake.png'
+import swapImage from './cardImages/swap.png'
 
 function Card({name, description, id, playCard}) {
     // determine image based on name
     return (
         <div>
             <div class='card' onClick={() => playCard(id)}>
-                <p id='title'>{name}</p>    
-                <p id='description'>{description}</p>
+                <img src={getSource(name)} />
             </div>
         </div>
 
     )
+
+    
 }
+
+const getSource = name => {
+    switch (name) {
+        case 'Freeze':
+            return snowflakeImage;
+        case 'Frozen Cross':
+            return frozenCrossImage
+        case 'Resurrect':
+            return reviveImage;
+        case 'Swap Hands':
+            return swapImage
+        case 'Demote piece':
+            return null
+        default:
+            return null
+    }
+}
+
 export default Card

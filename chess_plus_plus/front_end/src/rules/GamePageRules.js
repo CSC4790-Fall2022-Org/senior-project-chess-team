c3e3009740d45b5e42da428ab48db7ecc975e86f
Ryan Vaughan <ryvaughan17@gmail.com>
2022-12-04 13:11:37 -0500

    cleanup navbar

--- chess_plus_plus/front_end/src/rules/GamePageRules.js
+++ chess_plus_plus/front_end/src/rules/GamePageRules.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import './GamePageRules.css'
 function GamePageRules() {
-    const [tab, setTab] = useState(1);
+    const [tab, setTab] = useState(2);
 
     return (
         <div class="gamePageRules">
             <div class="tabs">
                 <ul class="tabsBar">
-                    <li>General</li>
-                    <li>Pieces</li>
-                    <li>Cards</li>
+                    <li class={tab === 1 ? "active" : ""} onClick={() => setTab(1)}>General</li>
+                    <li class={tab === 2 ? "active" : ""} onClick={() => setTab(2)}>Pieces</li>
+                    <li class={tab === 3 ? "active" : ""} onClick={() => setTab(3)}>Cards</li>
                 </ul>
             </div>
             <div class="rulesSection">
@@ -23,19 +23,19 @@ function GamePageRules() {
 
 const GeneralRules = () => {
     return (
-        <p>general</p>
+        <p style={{textAlign: 'center'}}>General</p>
     )
 }
 
 const PieceRules = () => {
     return (
-        <p>piece</p>
+        <p style={{textAlign: 'center'}}>Pieces</p>
     )
 }
 
 const CardRules = () => {
     return (
-        <p>cards</p>
+        <p style={{textAlign: 'center'}}>Cards</p>
     )
 }
 export default GamePageRules;

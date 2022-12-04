2f89caf5b35be1a03a088125cf09006b9b6b1760
Ryan Vaughan <ryvaughan17@gmail.com>
2022-12-04 13:59:02 -0500

    add pawn

--- chess_plus_plus/front_end/src/rules/GamePageRules.js
+++ chess_plus_plus/front_end/src/rules/GamePageRules.js
@@ -3,6 +3,7 @@ import './GamePageRules.css'
 
 import checkExample from '../chess/files/checkExample.png'
 import checkmateExample from '../chess/files/checkmateExample.png'
+import pawnExample from '../chess/files/pawnMoves.png'
 function GamePageRules() {
     const [tab, setTab] = useState(1);
 
@@ -29,23 +30,28 @@ const GeneralRules = () => {
         <div class='Rules'>
             <p class='subheader'>Check</p>
             <div class='ruleExample'>
-                <img id="checkExample" src={checkExample} alt='special square'/>
+                <img class="checkExample" src={checkExample} alt='special square'/>
                 <p  class="rule"> When a king is attacked. Black has 3 ways to exit check. You must make a move that removes your king from Check. You may not make a move that puts yourself in Check. </p>
             </div>
             <p class='subheader'>Checkmate</p>
             <div class='ruleExample'>
-                <img id="checkExample" src={checkmateExample} alt='special square'/>
+                <img class="checkExample" src={checkmateExample} alt='special square'/>
                 <p  class="rule"> When a king is attacked and the player has no valid moves. White is in checkmate and loses the game.</p>
             </div>
         </div>
-        
-
     )
 }
 
 const PieceRules = () => {
     return (
-        <p style={{textAlign: 'center'}}>Pieces</p>
+        <div class='Rules'>
+            <p class='subheader'>Pawn</p>
+            <div class='ruleExample'>
+                <img class="pieceExample" src={pawnExample} alt='special square'/>
+                <p  class="rule"> txt</p>
+            </div>
+            
+        </div>
     )
 }
 
